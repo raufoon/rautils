fa971d3225bc71ad1066c342cc6ddb77ddd22395
raufoon <128422316+raufoon@users.noreply.github.com>
2024-08-17 18:09:40 +0600

    Update rautils.py

--- rautils.py
+++ rautils.py
@@ -10,28 +10,31 @@ class FolderServer:
     _server_thread = None
     _httpd = None
     _port = None
-
+    _folder = None
     def __new__(cls, path=None):
         return cls._start_server(path)
 
     @classmethod
     def _start_server(cls, path=None):
         """Start the server and save the port number. Optionally set the directory path."""
-        if cls._httpd is None:
+        if cls._httpd is None or path != cls._folder:
+            cls._stop(no_output=True)
             cls._port = cls._find_free_port()
             #print(f"Found available port at {cls._port}")
             try:
                 # Set to the specified path or use the current working directory
                 if path is not None:
                     os.chdir(path)
+                    cls._folder = path
                 else:
                     os.chdir(os.getcwd())
+                    cls._folder = os.getcwd()
 
                 Handler = http.server.SimpleHTTPRequestHandler
                 cls._httpd = socketserver.TCPServer(("", cls._port), Handler)
                 cls._server_thread = threading.Thread(target=cls._httpd.serve_forever)
                 cls._server_thread.start()
-                print(f"Server started on port {cls._port} serving directory {os.getcwd()}")
+                print(f"Server started on port {cls._port} serving directory {cls._folder}")
             except Exception as e:
                 print(f"Failed to start server: {e}")
                 cls._httpd = None
@@ -48,12 +51,10 @@ class FolderServer:
     @classmethod
     def start(cls):
         """Start the server if not already running and return the port."""
-        if cls._httpd is None:
-            cls()
-        return cls
+        return cls()
 
     @classmethod
-    def stop(cls):
+    def stop(cls, no_output = False):
         """Stop the server and shutdown the server thread if running."""
         if cls._httpd is not None:
             cls._httpd.shutdown()
@@ -68,15 +69,11 @@ class FolderServer:
             res = ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, ctypes.py_object(SystemExit))
             if res > 1:
                 ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, 0)
-                print('Exception raise failure')
-        print(f"Server on port {cls.port} stopped.")
+                if not no_output:
+                    print('Exception raise failure')
+        if not no_output:
+            print(f"Server on port {cls.port} stopped.")
         return cls
-
-    @classmethod
-    def restart(cls):
-        """Restart the server."""
-        cls.stop()
-        return cls.start()
 
     @classmethod
     @property
@@ -89,4 +86,10 @@ class FolderServer:
     def url(cls) -> str:
         """Get the current url, or None if the server is not running."""
         return f"http://localhost:{cls._port}/"
+
+    @classmethod
+    @property
+    def serving_folder(cls) -> str:
+        """Get the current serving folder."""
+        return cls._folder
 #### ============= ####
