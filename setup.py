2603f13cc6b3754c2e94c34d37c37fd910b72983
raufoon <128422316+raufoon@users.noreply.github.com>
2024-08-17 16:54:19 +0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rautils',
+    version='0.1',
+    description='A library containing utility functions for personal projects',
+    packages=find_packages(), 
+    python_requires='>=3.6', 
+)
