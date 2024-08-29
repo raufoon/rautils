b6fe997a4dab4618c3db241d1123719cc8d5485e
raufoon <128422316+raufoon@users.noreply.github.com>
2024-08-29 12:43:29 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rautils',
-    version='0.1',
+    version='0.11',
     py_modules=['rautils'],
     description='A library containing utility functions for personal projects',
     packages=find_packages(), 
